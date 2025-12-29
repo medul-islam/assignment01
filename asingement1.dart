d93e8fdda21226bcb67a93a59a8bc58883146732
Md Medul Islam <md.medulislam200@gmail.com>
2025-12-29 21:32:04 +0600

    Assingement1
    Assingement1

---
+++ asingement1.dart
@@ -0,0 +1,13 @@
+void main(){
+  Set<int> A = {1,2,3,4,5};
+  Set<int> B = {4,5,6,7,8};
+
+  for(int item1 in A){
+    print("set A $item1");
+  }
+  for(int item2 in B){
+    print("set B $item2");
+  }
+
+
+}
